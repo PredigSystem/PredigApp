337facc747f8b2aa16473080f672a7eaa7cf5edb
Pau Balaguer <pbalaguer19@gmail.com>
2017-10-24 18:20:00 +0200

    NavigationMenu working for MainActivity. Profile elements hardcoded. #8

--- app/src/main/java/predigsystem/udl/org/predigsystem/HomeActivity.java
+++ app/src/main/java/predigsystem/udl/org/predigsystem/HomeActivity.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -19,23 +26,45 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName(R.string.home);
-        SecondaryDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(2).withName(R.string.dashboard);
-        SecondaryDrawerItem item3 = new SecondaryDrawerItem().withIdentifier(3).withName(R.string.map);
+        // Menu Items
+        SecondaryDrawerItem home = new SecondaryDrawerItem().withIdentifier(1).withName(R.string.home).withIcon(GoogleMaterial.Icon.gmd_home);
+        SecondaryDrawerItem dashboard = new SecondaryDrawerItem().withIdentifier(2).withName(R.string.dashboard).withIcon(FontAwesome.Icon.faw_line_chart);
+        SecondaryDrawerItem map = new SecondaryDrawerItem().withIdentifier(3).withName(R.string.map).withIcon(FontAwesome.Icon.faw_map_marker);
+        SecondaryDrawerItem settings = new SecondaryDrawerItem().withName(R.string.settings).withIcon(GoogleMaterial.Icon.gmd_settings);
+        SecondaryDrawerItem logout = new SecondaryDrawerItem().withName(R.string.logout).withIcon(FontAwesome.Icon.faw_sign_out);
 
+        // Menu Header
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Pau Balaguer").withEmail("pbl5@alumnes.udl.cat").withIcon(getResources().getDrawable(R.drawable.user))
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+                        return false;
+                    }
+                })
+                .build();
+
+        // Menu
         Drawer result = new DrawerBuilder()
                 .withActivity(this)
                 .withToolbar(toolbar)
+                .withAccountHeader(headerResult)
                 .withActionBarDrawerToggle(true)
                 .withActionBarDrawerToggleAnimated(true)
                 .addDrawerItems(
-                        item1,
-                        new DividerDrawerItem(),
-                        item2,
-                        item3,
-                        new SecondaryDrawerItem().withName(R.string.settings)
+                        home,
+                        dashboard,
+                        map,
+                        new SectionDrawerItem().withName(R.string.other),
+                        settings,
+                        logout
                 )
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
@@ -45,5 +74,6 @@ public class HomeActivity extends AppCompatActivity {
                     }
                 })
                 .build();
+
     }
 }
