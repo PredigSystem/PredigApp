fbce31833c0aea76245bd89e877c07336637e3b9
Sultan <sultanmahmut.beisen@gmail.com>
2017-10-30 19:34:23 +0100

    BT Connection views

--- app/src/main/java/predigsystem/udl/org/predigsystem/Fragments/HomeFragment.java
+++ app/src/main/java/predigsystem/udl/org/predigsystem/Fragments/HomeFragment.java
@@ -1,16 +1,20 @@
 package predigsystem.udl.org.predigsystem.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import predigsystem.udl.org.predigsystem.BTConnectionActivity;
+import predigsystem.udl.org.predigsystem.BloodPressureMeasurementActivity;
 import predigsystem.udl.org.predigsystem.R;
 
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener{
 
     public HomeFragment() {
     }
@@ -19,5 +23,19 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Button btn = getActivity().findViewById(R.id.btn_start);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(getContext(), BTConnectionActivity.class);
+        startActivity(intent);
     }
 }
