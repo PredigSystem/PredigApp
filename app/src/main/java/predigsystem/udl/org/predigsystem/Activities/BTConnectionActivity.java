56f0ef34e55a4b11f25e051edaf98ea3d2cfacc6
Pau Balaguer <pbalaguer19@gmail.com>
2017-12-04 19:14:26 +0100

    Device connection started. Not working

--- app/src/main/java/predigsystem/udl/org/predigsystem/Activities/BTConnectionActivity.java
+++ app/src/main/java/predigsystem/udl/org/predigsystem/Activities/BTConnectionActivity.java
@@ -1,18 +1,30 @@
 package predigsystem.udl.org.predigsystem.Activities;
 
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
 
+import com.ihealth.communication.manager.iHealthDevicesManager;
+import com.ihealth.communication.manager.iHealthDevicesCallback;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
 import predigsystem.udl.org.predigsystem.R;
 
 public class BTConnectionActivity extends AppCompatActivity {
+
+    private final static int BLUETOOTH_ENABLED = 1;
+    private String userName = "pbalaguer19@gmail.com";
+    private String clientId = "274148308bb64d2db1153a3dddd85476";
+    private String clientSecret = "56214c8ecd8f4d85905ef56d8bc37206";
+    private Boolean bluetoothReady = false;
+    private ProgressDialog progressDialog;
+    private Class selectedDeviceClass;
+    private String selectedDeviceType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +49,63 @@ public class BTConnectionActivity extends AppCompatActivity {
                 }
             }
         });
+
+        checkBluetooth();
+        iHealthDevicesManager.getInstance().init(BTConnectionActivity.this);
+        int callbackId = iHealthDevicesManager.getInstance().registerClientCallback(iHealthDevicesCallback);
+        iHealthDevicesManager.getInstance().sdkUserInAuthor(BTConnectionActivity.this, userName, clientId, clientSecret, callbackId);
+
+        if(bluetoothReady) {
+            int type = iHealthDevicesManager.DISCOVERY_BP5;
+            iHealthDevicesManager.getInstance().startDiscovery(type);
+        } else {
+            Toast.makeText(this,"Bluetooth is not ready!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private iHealthDevicesCallback iHealthDevicesCallback = new iHealthDevicesCallback() {
+
+        @Override
+        public void onScanDevice(String mac, String deviceType) {
+            /*if (selectedDeviceType.equals(deviceType)) {
+                iHealthDevicesManager.getInstance().stopDiscovery();
+                iHealthDevicesManager.getInstance().connectDevice(userName, mac);
+            }*/
+        }
+
+        @Override
+        public void onDeviceConnectionStateChange(String mac, String deviceType, int status) {
+            /*if (selectedDeviceType.equals(deviceType) && status==1) {
+                Intent intent = new Intent();
+                intent.putExtra("mac", mac);
+                intent.setClass(MainActivity.this, selectedDeviceClass);
+                startActivityForResult(intent, DEVICE);
+            }*/
+        }
+
+        @Override
+        public void onUserStatus(String username, int userStatus) {}
+
+        @Override
+        public void onDeviceNotify(String mac, String deviceType, String action, String message) {}
+
+        @Override
+        public void onScanFinish() {
+            //progressDialog.dismiss();
+        }
+    };
+
+    private void checkBluetooth() {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+           Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_SHORT).show();
+        } else if (!bluetoothAdapter.isEnabled()) {
+            // Bluetooth is not enable
+            Toast.makeText(this, "Bluetooth not enabled", Toast.LENGTH_SHORT).show();
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent,BLUETOOTH_ENABLED);
+        } else {
+            bluetoothReady = true;
+        }
     }
 }
