c076774a67fe42041e54d78a7fda8b15bc4a3f10
didacflorensa <didacflorensacazorla@gmail.com>
2017-12-12 14:15:23 +0100

    Implements the calendarFragment to get information from the api

--- app/src/main/java/predigsystem/udl/org/predigsystem/Fragments/CalendarFragment.java
+++ app/src/main/java/predigsystem/udl/org/predigsystem/Fragments/CalendarFragment.java
@@ -1,6 +1,8 @@
 package predigsystem.udl.org.predigsystem.Fragments;
 
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,18 +11,41 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
 import pl.rafman.scrollcalendar.ScrollCalendar;
 import pl.rafman.scrollcalendar.contract.DateWatcher;
 import pl.rafman.scrollcalendar.contract.MonthScrollListener;
 import pl.rafman.scrollcalendar.contract.OnDateClickListener;
 import pl.rafman.scrollcalendar.data.CalendarDay;
+import predigsystem.udl.org.predigsystem.Database.PredigAppDB;
+import predigsystem.udl.org.predigsystem.Interfaces.PredigAPIService;
+import predigsystem.udl.org.predigsystem.JavaClasses.VisitsDoctor;
 import predigsystem.udl.org.predigsystem.R;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CalendarFragment extends Fragment{
 
+    SQLiteDatabase db;
+    private Call<VisitsDoctor> nextVisit = null;
+    private Call<List<VisitsDoctor>> visitsList;
 
     public CalendarFragment() {
         // Required empty public constructor
@@ -37,6 +62,12 @@ public class CalendarFragment extends Fragment{
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        PredigAppDB predigAppDB = new PredigAppDB(getActivity(), "PredigAppDB", null, 1);
+        db = predigAppDB.getReadableDatabase();
+
+        getVisits("00000000X");
+        getAPIInformation("uid123");
 
         ScrollCalendar scrollCalendar = (ScrollCalendar) getActivity().findViewById(R.id.scrollCalendar);
         scrollCalendar.setOnDateClickListener(new OnDateClickListener() {
@@ -68,5 +99,43 @@ public class CalendarFragment extends Fragment{
                 return true;
             }
         });
+    }
+
+    public void getVisits (String nif) {
+        Cursor consult = db.rawQuery("SELECT Date, Time FROM VisitsDoctor WHERE NIF='"+nif+"';", null);
+
+        if(consult.moveToFirst())
+            do {
+                String date = consult.getString(0);
+                String time = consult.getString(1);
+                System.out.println(date + " " + time);
+            }while(consult.moveToNext());
+
+    }
+
+    private void getAPIInformation(String user){
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(java.sql.Date.class, new JsonDeserializer<Date>() {
+            @Override
+            public java.sql.Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+                DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+                try {
+                    return new java.sql.Date(df.parse(json.getAsString()).getTime());
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        });
+        Gson gson = gsonBuilder.create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:8080/predig/api/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        PredigAPIService service = retrofit.create(PredigAPIService.class);
+
+        nextVisit = service.lastVisitsDoctorByUser(user);
+        visitsList = service.visitsDoctorByUser(user);
     }
 }
