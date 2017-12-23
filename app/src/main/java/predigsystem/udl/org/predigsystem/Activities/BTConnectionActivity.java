d21934568805548d631fac8098eba7f70920baed
Pau Balaguer <pbalaguer19@gmail.com>
2017-12-23 18:17:06 +0100

    Device connection done. Not working start Measure

--- app/src/main/java/predigsystem/udl/org/predigsystem/Activities/BTConnectionActivity.java
+++ app/src/main/java/predigsystem/udl/org/predigsystem/Activities/BTConnectionActivity.java
@@ -1,18 +1,25 @@
 package predigsystem.udl.org.predigsystem.Activities;
 
-import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
-import com.example.smartlinklib.MainActivity;
+import com.ihealth.communication.control.Bp5Control;
+import com.ihealth.communication.control.BpProfile;
 import com.ihealth.communication.manager.iHealthDevicesManager;
 import com.ihealth.communication.manager.iHealthDevicesCallback;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Map;
 
@@ -20,14 +27,16 @@ import predigsystem.udl.org.predigsystem.R;
 
 public class BTConnectionActivity extends AppCompatActivity {
 
-    private final static int BLUETOOTH_ENABLED = 1;
+    private static final int BLUETOOTH_ENABLED = 1;
+    private static final String selectedDeviceType = "BP5";
+    private static final String TAG = "BP_TAG";
     private String userName = "pbalaguer19@gmail.com";
     private String clientId = "274148308bb64d2db1153a3dddd85476";
     private String clientSecret = "56214c8ecd8f4d85905ef56d8bc37206";
     private Boolean bluetoothReady = false;
-    private ProgressDialog progressDialog;
-    private Class selectedDeviceClass;
-    private String selectedDeviceType;
+    private Bp5Control bp5Control;
+    private String deviceMac = null;
+    private int callbackId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,9 +63,10 @@ public class BTConnectionActivity extends AppCompatActivity {
         });
 
         checkBluetooth();
-        iHealthDevicesManager.getInstance().init(BTConnectionActivity.this);
-        int callbackId = iHealthDevicesManager.getInstance().registerClientCallback(iHealthDevicesCallback);
+        iHealthDevicesManager.getInstance().init(this, 2, 2);
+        callbackId = iHealthDevicesManager.getInstance().registerClientCallback(iHealthDevicesCallback);
         iHealthDevicesManager.getInstance().sdkUserInAuthor(BTConnectionActivity.this, userName, clientId, clientSecret, callbackId);
+        iHealthDevicesManager.getInstance().addCallbackFilterForDeviceType(callbackId, iHealthDevicesManager.TYPE_BP5);
 
         if(bluetoothReady) {
             long type = iHealthDevicesManager.DISCOVERY_BP5;
@@ -66,55 +76,196 @@ public class BTConnectionActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        iHealthDevicesManager.getInstance().unRegisterClientCallback(callbackId);
+    }
+
+    private void connect(){
+        bp5Control = iHealthDevicesManager.getInstance().getBp5Control(deviceMac);
+
+        if(bp5Control != null)
+            bp5Control.startMeasure();
+        else
+            Toast.makeText(this, "Error on connect to Blood Pressure Device!", Toast.LENGTH_LONG).show();
+    }
+
     private iHealthDevicesCallback iHealthDevicesCallback = new iHealthDevicesCallback() {
 
         @Override
         public void onScanDevice(String mac, String deviceType, int rssi) {
             super.onScanDevice(mac, deviceType, rssi);
-            onScanDevice(mac, deviceType);
+            deviceMac = mac;
         }
 
         @Override
         public void onScanDevice(String mac, String deviceType, int rssi, Map manufactorData) {
             super.onScanDevice(mac, deviceType, rssi, manufactorData);
-            onScanDevice(mac, deviceType);
-        }
-
-        @Override
-        public void onDeviceConnectionStateChange(String mac, String deviceType, int status, int errorID) {
-            super.onDeviceConnectionStateChange(mac, deviceType, status, errorID);
-        }
-
-        @Override
-        public void onDeviceConnectionStateChange(String mac, String deviceType, int status, int errorID, Map manufactorData) {
-            super.onDeviceConnectionStateChange(mac, deviceType, status, errorID, manufactorData);
-        }
-
-        private void onScanDevice(String mac, String deviceType) {
-            if (selectedDeviceType.equals(deviceType)) {
-                iHealthDevicesManager.getInstance().stopDiscovery();
-                iHealthDevicesManager.getInstance().connectDevice(userName, mac);
-            }
-        }
-
-        public void onDeviceConnectionStateChange(String mac, String deviceType, int status) {
-            if (selectedDeviceType.equals(deviceType) && status==1) {
-                /*Intent intent = new Intent();
-                intent.putExtra("mac", mac);
-                intent.setClass(getApplicationContext(), selectedDeviceClass);
-                startActivityForResult(intent, DEVICE);*/
-            }
+            deviceMac = mac;
         }
 
         @Override
         public void onUserStatus(String username, int userStatus) {}
 
+        //TODO: Change things, add Toasts!!!
         @Override
-        public void onDeviceNotify(String mac, String deviceType, String action, String message) {}
+        public void onDeviceNotify(String mac, String deviceType, String action, String message) {
+            Log.i(TAG, "mac: " + mac);
+            Log.i(TAG, "deviceType: " + deviceType);
+            Log.i(TAG, "action: " + action);
+            Log.i(TAG, "message: " + message);
+
+            if(BpProfile.ACTION_BATTERY_BP.equals(action)){
+                try {
+                    JSONObject info = new JSONObject(message);
+                    String battery = info.getString(BpProfile.BATTERY_BP);
+                    Message msg = new Message();
+                    msg.what = HANDLER_MESSAGE;
+                    msg.obj = "battery: " + battery;
+                    myHandler.sendMessage(msg);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }else if(BpProfile.ACTION_DISENABLE_OFFLINE_BP.equals(action)){
+                Log.i(TAG, "disable operation is success");
+
+            }else if(BpProfile.ACTION_ENABLE_OFFLINE_BP.equals(action)){
+                Log.i(TAG, "enable operation is success");
+
+            }else if(BpProfile.ACTION_ERROR_BP.equals(action)){
+                try {
+                    JSONObject info = new JSONObject(message);
+                    String num =info.getString(BpProfile.ERROR_NUM_BP);
+                    Message msg = new Message();
+                    msg.what = HANDLER_MESSAGE;
+                    msg.obj = "error num: " + num;
+                    myHandler.sendMessage(msg);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }else if(BpProfile.ACTION_HISTORICAL_DATA_BP.equals(action)){
+                String str = "";
+                try {
+                    JSONObject info = new JSONObject(message);
+                    if (info.has(BpProfile.HISTORICAL_DATA_BP)) {
+                        JSONArray array = info.getJSONArray(BpProfile.HISTORICAL_DATA_BP);
+                        for (int i = 0; i < array.length(); i++) {
+                            JSONObject obj = array.getJSONObject(i);
+                            String date          = obj.getString(BpProfile.MEASUREMENT_DATE_BP);
+                            String hightPressure = obj.getString(BpProfile.HIGH_BLOOD_PRESSURE_BP);
+                            String lowPressure   = obj.getString(BpProfile.LOW_BLOOD_PRESSURE_BP);
+                            String pulseWave     = obj.getString(BpProfile.PULSE_BP);
+                            String ahr           = obj.getString(BpProfile.MEASUREMENT_AHR_BP);
+                            String hsd           = obj.getString(BpProfile.MEASUREMENT_HSD_BP);
+                            str = "date:" + date
+                                    + "hightPressure:" + hightPressure + "\n"
+                                    + "lowPressure:" + lowPressure + "\n"
+                                    + "pulseWave" + pulseWave + "\n"
+                                    + "ahr:" + ahr + "\n"
+                                    + "hsd:" + hsd + "\n";
+                        }
+                    }
+                    Message msg = new Message();
+                    msg.what = HANDLER_MESSAGE;
+                    msg.obj =  str;
+                    myHandler.sendMessage(msg);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }else if(BpProfile.ACTION_HISTORICAL_NUM_BP.equals(action)){
+                try {
+                    JSONObject info = new JSONObject(message);
+                    String num = info.getString(BpProfile.HISTORICAL_NUM_BP);
+                    Message msg = new Message();
+                    msg.what = HANDLER_MESSAGE;
+                    msg.obj = "num: " + num;
+                    myHandler.sendMessage(msg);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }else if(BpProfile.ACTION_IS_ENABLE_OFFLINE.equals(action)){
+                try {
+                    JSONObject info = new JSONObject(message);
+                    String isEnableoffline =info.getString(BpProfile.IS_ENABLE_OFFLINE);
+                    Message msg = new Message();
+                    msg.what = HANDLER_MESSAGE;
+                    msg.obj = "isEnableoffline: " + isEnableoffline;
+                    myHandler.sendMessage(msg);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }else if(BpProfile.ACTION_ONLINE_PRESSURE_BP.equals(action)){
+                try {
+                    JSONObject info = new JSONObject(message);
+                    String pressure =info.getString(BpProfile.BLOOD_PRESSURE_BP);
+                    Message msg = new Message();
+                    msg.what = HANDLER_MESSAGE;
+                    msg.obj = "pressure: " + pressure;
+                    myHandler.sendMessage(msg);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }else if(BpProfile.ACTION_ONLINE_PULSEWAVE_BP.equals(action)){
+                try {
+                    JSONObject info = new JSONObject(message);
+                    String pressure =info.getString(BpProfile.BLOOD_PRESSURE_BP);
+                    String wave = info.getString(BpProfile.PULSEWAVE_BP);
+                    String heartbeat = info.getString(BpProfile.FLAG_HEARTBEAT_BP);
+                    Message msg = new Message();
+                    msg.what = HANDLER_MESSAGE;
+                    msg.obj = "pressure:" + pressure + "\n"
+                            + "wave: " + wave + "\n"
+                            + " - heartbeat:" + heartbeat;
+                    myHandler.sendMessage(msg);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }else if(BpProfile.ACTION_ONLINE_RESULT_BP.equals(action)){
+                try {
+                    JSONObject info = new JSONObject(message);
+                    String highPressure =info.getString(BpProfile.HIGH_BLOOD_PRESSURE_BP);
+                    String lowPressure =info.getString(BpProfile.LOW_BLOOD_PRESSURE_BP);
+                    String ahr =info.getString(BpProfile.MEASUREMENT_AHR_BP);
+                    String pulse =info.getString(BpProfile.PULSE_BP);
+                    Message msg = new Message();
+                    msg.what = HANDLER_MESSAGE;
+                    msg.obj = "highPressure: " + highPressure
+                            + "lowPressure: " + lowPressure
+                            + "ahr: " + ahr
+                            + "pulse: " + pulse;
+                    myHandler.sendMessage(msg);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }else if(BpProfile.ACTION_ZOREING_BP.equals(action)){
+                Message msg = new Message();
+                msg.what = HANDLER_MESSAGE;
+                msg.obj = "zoreing";
+                myHandler.sendMessage(msg);
+
+            }else if(BpProfile.ACTION_ZOREOVER_BP.equals(action)){
+                Message msg = new Message();
+                msg.what = HANDLER_MESSAGE;
+                msg.obj = "zoreover";
+                myHandler.sendMessage(msg);
+
+            }
+
+        }
 
         @Override
         public void onScanFinish() {
-            progressDialog.dismiss();
+            connect();
         }
     };
 
@@ -131,4 +282,16 @@ public class BTConnectionActivity extends AppCompatActivity {
             bluetoothReady = true;
         }
     }
+
+    private static final int HANDLER_MESSAGE = 101;
+    Handler myHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case HANDLER_MESSAGE:
+                    //tv_return.setText((String)msg.obj);
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
 }
